7c716d8dfd735912c0325626f90604856b8eddb1
Roberto Castro <roberto.ernesto.castro@gmail.com>
2019-11-25 15:38:39 -0600

    Uso de CORS implementado

--- server.js
+++ server.js
@@ -2,7 +2,10 @@ var express = require('express'),
     app = express(),
     port = process.env.PORT || 3001;
 
+const cors = require('cors');
+
 app.listen(port);
+app.use(cors());
 
 app.use('/api/plugina', require('./api/routes/routes'));
 
